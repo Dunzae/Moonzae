a3f6a8628192297f714c0cd5105463c85b8184fa
dunzae <dunzae99@gmail.com>
2024-11-04 14:24:55 +0900

    @Feat ServerSocket 클래스 생성

---
+++ Socket/ServerSocket.h
@@ -0,0 +1,34 @@
+#ifndef __SOCKET_H__
+#define __SOCKET_H___
+
+#include <WinSock2.h>
+#include <ws2tcpip.h>
+#include <iostream>
+#include <string>
+#include "Socket.h"
+
+class ServerSocket : public Socket
+{
+private:
+    SOCKET listenSocket;
+    
+
+public:
+    ServerSocket(std::string _ip) : Socket{_ip}{};
+    ~ServerSocket() {};
+
+    bool createListenSocket();
+    bool bind();
+    bool listen();
+    bool accept();
+    bool sendMessage(std::string message) override;
+    std::string recvMessage(unsigned int maxSize) override;
+    bool shutdownClient();
+
+    //Getter and Setter
+    SOCKET GetListenSocket() const;
+    void SetListenSocket(SOCKET listenSocket);
+    
+};
+
+#endif
